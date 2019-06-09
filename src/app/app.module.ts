38dae55fc03ff9cc8668abe929efd4b3cf94500e
Your Name <rsmith5901@gmail.com>
2019-06-09 18:39:02 -0400

    asdffdsadfsa

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule, Injector, DoBootstrap } from '@angular/core';
+import { createCustomElement } from '@angular/elements';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule],
+  providers: [],
+  entryComponents: [AppComponent]
+})
+export class AppModule implements DoBootstrap {
+  constructor(private injector: Injector) {}
+
+  ngDoBootstrap() {
+    const element = createCustomElement(AppComponent, { injector: this.injector });
+
+    customElements.define('app-test', element);
+  }
+}
